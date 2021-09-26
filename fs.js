28672a04cd76ce9e04b6a1d43c584b69b4393290
Kanishk <kanishkrawat67@gmail.com>
2021-09-26 22:19:49 +0530

    commit-1

--- fs.js
+++ fs.js
@@ -1,10 +1,10 @@
 let fs = require("fs");
 
-// let obj = {
-//     name : "Kanishk",
-//     Salary : "8LPA/yr",
-//     Age : "25"
-// }
+let obj = {
+    name : "Kanishk",
+    Salary : "8LPA/yr",
+    Age : "25"
+}
 
 // fs.writeFileSync("./file.txt", "I'm  good");
 // // let objString =  JSON.stringify(obj);
@@ -14,12 +14,19 @@ let fs = require("fs");
 // console.log(JSON.parse(data));
 
 
-let cheerio = require("cheerio");
+// let cheerio = require("cheerio");
 
-let htmlData = fs.readFileSync("index.html","UTF-8");
-// console.log(htmlData);
+// let htmlData = fs.readFileSync("index.html","UTF-8");
+// // console.log(htmlData);
 
-let myDoc = cheerio.load(htmlData);
-let h1Text = myDoc("h1").text();
-console.log(h1Text);
-console.log(h1Text);
+// let myDoc = cheerio.load(htmlData);
+// let h1Text = myDoc("h1").text();
+// console.log(h1Text);
+// console.log(h1Text);
+
+
+let keysArr = Object.keys(obj);
+console.log(keysArr);
+for(let key in obj){
+    console.log(obj[key]);
+}
