65024458563d5231c02f13d36501d5e74dcf11b5
Kanishk <kanishkrawat67@gmail.com>
2021-10-11 23:42:13 +0530

    object &barray methods

---
+++ Front-End Interview Questions/deepCopyvsShallowCOpy.js
@@ -0,0 +1,57 @@
+var user = {
+    name: "Vishal",
+    address: {
+      primary: {
+        house: "109",
+        street: {             
+          main: "21",
+          cross: "32"
+        }
+      }
+    }
+  };
+
+  let keys  = Object.keys(user); // will give an array of keys
+//   console.log(keys);
+
+
+
+// this gives an object it basically creates shallow copy of it , means if we make any change to copiedobj it will not reflect in user
+  let copiedObj = Object.assign({},user); 
+  copiedObj.address = "xyz"; // copied object will change but user object will not
+  console.log(copiedObj);
+
+//----------------------------------------------    
+
+let person = user;
+person["age"] = 23;
+person.married = "no"
+// console.log(person);
+// console.log(Object.keys(person).length);
+
+//---------------------------------------------- 
+// its an exaample of deep copy since newUser and user they both are pointing to the same memory location so whatever change will occur on any of 
+// them will reflect on both of these either deleteion of key , deletion.
+
+let newUser = user;
+user.name="Rahul"
+user["name"]="Binod"
+// console.log(newUser);
+// console.log(user);
+
+//----------------------------------------------
+
+//Object.entries bascially gives an array of key,value
+let keyValueArr = Object.entries(user);
+// console.log(keyValueArr);
+
+// Object.values is an array of values in a given object
+let value = Object.values(user);
+// console.log(value);
+
+let arr= [1,2,3,4,5,6,7,8];
+arr.forEach((val)=> console.log(val));
+
+for(const val of arr){
+    console.log(val);
+}
--- Front-End Interview Questions/flattenObj.js
+++ Front-End Interview Questions/flattenObj.js
@@ -10,22 +10,48 @@ var user = {
       }
     }
   };
-  
-  let result  = {};
-  let keyName = "user";
 
-  var flattenObj = function(user, keyName){
-    Object.keys(user).forEach((key)=>{
-        var newKey = `${keyName}_${key}`;
+//---------------------------------------------- 8i810
+// Flatten an object : -
+//   let result  = {};
+//   let keyName = "user";
+
+//   const flattenObj = function(user, keyName){
+//     Object.keys(user).forEach((key)=>{
+//         var newKey = `${keyName}_${key}`;
         
-        if(typeof(user[key]) === 'object'){
-          flattenObj(user[key],newKey);
-        } else{
-          result[newKey] = user[key];
-        }
-      })
-      return result;
-  }
+//         if(typeof(user[key]) === 'object'){
+//           flattenObj(user[key],newKey);
+//         } else{
+//           result[newKey] = user[key];
+//         }
+//       })
+//       return result;
+//   }
   
-  let ans  = flattenObj(user,keyName);
-  console.log(ans);
+//   let ans  = flattenObj(user,keyName);
+//   console.log(ans);
+
+
+  let obj = {
+    user_name: 'Vishal',
+    user_address_primary_house: '109',
+    user_address_primary_street_main: '21',
+    user_address_primary_street_cross: '32'
+  }
+
+  let o = Object.keys(obj);
+  let ans = o[0].split('_');
+//   console.log(ans[0]);
+  let keyName = ans.slice(1);
+  console.log(keyName);
+
+
+
+const deflatten = function(obj){
+
+   for(let key in obj){
+       
+   }
+}
+
